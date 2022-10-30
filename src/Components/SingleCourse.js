8e8ee79fca473ce61d8aa4377a243cd85157fd63
droidmaximus <avinash.b20@iiits.in>
2022-10-31 00:29:44 +0530

    coursepage commit

--- src/Components/SingleCourse.js
+++ src/Components/SingleCourse.js
@@ -2,43 +2,38 @@ import React from 'react';
 import classes from "./SingleCourse.module.css";
 import Navb from "./Navb";
 import Footer from "./UI/Footer";
+import { useState } from 'react';
+import bootstrap from 'bootstrap';
 
 const SingleCourse = (props) => {
-    const {image , title , description , author , rating, students , hours , lecture , level} = props.course;
-    return (
+    const {image , title , description, contentList} = props.course;
+    const [show , setShow] = useState(false);
+    const handleShow = () => {
+        setShow(!show);
+    }
+    return(
       <>
-        <Navb></Navb>
-        <div className="row main-area">
-          <flexbox>
-            <div className="col-md-6">
-              <img src={image} alt="" className={classes.banner} />
+        <Navb />
+        <div className={classes.main}>
+          <div className={classes.course}>
+            <div className={classes.course__image}>
+              <img src={image} alt="course" />
             </div>
-          </flexbox>
-          <div className="col-md-8 pr-5">
-            <h5>{title}</h5>
-            <p>{description}</p>
-            <div className="row">
-              <div className="col-md-8">
-                <h6>
-                  {" "}
-                  <span>by: </span> {author}
-                </h6>
-                <h6>
-                  {" "}
-                  <b>{rating}</b> user rating ({students} students)
-                </h6>
-                <ul>
-                  <li>{hours} total hours,</li>
-                  <li>{lecture} lectures,</li>
-                  <li>{level} levels</li>
-                </ul>
-              </div>
+            <div className={classes.course__content}>
+              <h1>{title}</h1>
+              <p>{description}</p>
+              <button onClick={handleShow}>Show Content</button>
+              {show && <ul>
+                {contentList.map((content) => {
+                  return <li>{content}</li>;
+                })}
+              </ul>}
             </div>
           </div>
         </div>
-        <Footer></Footer>
+        <Footer />
       </>
     );
-};
 
+}
 export default SingleCourse;
