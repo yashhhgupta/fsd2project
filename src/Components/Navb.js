4f2c3184e9e6d917c734f8ba67de22a0a2116735
Yash Gupta <yashguptaaa333@gmail.com>
2022-10-30 15:33:32 +0530

    fixed colors of navbar

--- src/Components/Navb.js
+++ src/Components/Navb.js
@@ -14,31 +14,66 @@ function Navb() {
     <>
       <Navbar className={classes.NavbarMain}>
         <Container>
-          <Navbar.Brand> 
-            <Link to="/" style={{textDecoration: 'none'}} className={classes.heading}>
-            <img
-              alt=""
-              src="https://images-platform.99static.com//07LvaO3g4kyq6fDJNuXL2Tz6mCQ=/162x151:2074x2063/fit-in/500x500/projects-files/127/12734/1273474/3ce6ead9-abd5-499e-ab2d-64c7190c912e.jpg"
-              width="30"
-              height="30"
-              className="d-inline-block align-top"
-            />
-            {"      "}
-            HowToBasic
+          <Navbar.Brand>
+            <Link
+              to="/"
+              style={{ textDecoration: "none" }}
+              className={classes.heading}
+            >
+              <img
+                alt=""
+                src="https://images-platform.99static.com//07LvaO3g4kyq6fDJNuXL2Tz6mCQ=/162x151:2074x2063/fit-in/500x500/projects-files/127/12734/1273474/3ce6ead9-abd5-499e-ab2d-64c7190c912e.jpg"
+                width="30"
+                height="30"
+                className="d-inline-block align-top"
+              />
+              {"      "}
+              <span style={{ color: "black" }}>
+                <strong>HowToBasic</strong>
+              </span>
             </Link>
           </Navbar.Brand>
           <Nav>
-            <Link to="/Course" className={classes.headings1}>Courses</Link>
-            <Link to="/RequestForm" className={classes.headings1}>Request a Course</Link>
+            <Link
+              to="/Course"
+              className={classes.headings1}
+              style={{ textDecoration: "none" }}
+            >
+              <div className={classes.headings1}>
+                <span style={{ color: "black" }}>
+                  <b>Courses</b>
+                </span>
+              </div>
+            </Link>
+            <Link to="/RequestForm" style={{ textDecoration: "none" }}>
+              <div className={classes.headings1}>
+                <span style={{ color: "black" }}>
+                  <b>Request a Course</b>
+                </span>
+              </div>
+            </Link>
             {ctx.isLoggedInAd && (
-            <Link to="/AddCourse" className={classes.headings1}>Add a Course</Link>
+              <Link
+                to="/AddCourse"
+                className={classes.headings1}
+                style={{ textDecoration: "none" }}
+              >
+                <div className={classes.headings1}>
+                  <span style={{ color: "black" }}>
+                    <b>Add a course</b>
+                  </span>
+                </div>
+              </Link>
             )}
           </Nav>
           <Nav>
-            <Button variant="info" onClick={ctx.onLogout} className={classes.Button}>
+            <Button
+              variant="info"
+              onClick={ctx.onLogout}
+              className={classes.Button}
+            >
               Logout
             </Button>
-            
           </Nav>
         </Container>
       </Navbar>
