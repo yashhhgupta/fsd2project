1104e3c45128ce777f1be76ab839c242346a2b41
Yash Gupta <yashguptaaa333@gmail.com>
2022-10-30 12:47:25 +0530

    changed navbar

--- src/Components/Navb.js
+++ src/Components/Navb.js
@@ -14,7 +14,8 @@ function Navb() {
     <>
       <Navbar className={classes.NavbarMain}>
         <Container>
-          <Navbar.Brand href="/" className={classes.heading}>
+          <Navbar.Brand> 
+            <Link to="/" style={{textDecoration: 'none'}} className={classes.heading}>
             <img
               alt=""
               src="https://images-platform.99static.com//07LvaO3g4kyq6fDJNuXL2Tz6mCQ=/162x151:2074x2063/fit-in/500x500/projects-files/127/12734/1273474/3ce6ead9-abd5-499e-ab2d-64c7190c912e.jpg"
@@ -24,11 +25,14 @@ function Navb() {
             />
             {"      "}
             HowToBasic
+            </Link>
           </Navbar.Brand>
           <Nav>
-            <Nav.Link href="/Course" className={classes.headings1}>Courses</Nav.Link>
-            <Nav.Link href="/RequestForm" className={classes.headings1}>Request a Course</Nav.Link>
-            <Nav.Link href="/AddCourse" className={classes.headings1}>Add a Course</Nav.Link>
+            <Link to="/Course" className={classes.headings1}>Courses</Link>
+            <Link to="/RequestForm" className={classes.headings1}>Request a Course</Link>
+            {ctx.isLoggedInAd && (
+            <Link to="/AddCourse" className={classes.headings1}>Add a Course</Link>
+            )}
           </Nav>
           <Nav>
             <Button variant="info" onClick={ctx.onLogout} className={classes.Button}>
