803e28795cff1593a03aa689bcf506b3c02e47e1
Yash Gupta <yashguptaaa333@gmail.com>
2022-12-04 15:16:02 +0530

    a

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -66,7 +66,7 @@ const Home = (props) => {
       // const data = response.json();
     };
     fetchitems();
-  }, [registerCourseHandler]);
+  }, []);
   useEffect(() => {
     const fetchitems = () => {
       const uid = localStorage.getItem('userId');
